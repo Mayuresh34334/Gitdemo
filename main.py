bee158146fe123aeaf99178a65c63b17a14f3bb2
Rujuta B <93034566+rujuta13@users.noreply.github.com>
2023-05-03 22:15:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from tkinter import *
 import sqlite3
+from PIL import ImageTk, Image
 
 #connecting to database
-try:
-    conn = sqlite3.connect('Project/ExpenseTracker.db') #database path
-    cur = conn.cursor()
-except:
-    print("Unable to connect to database")
+#try:
+conn = sqlite3.connect('ExpenseTracker.db') #database path
+cur = conn.cursor()
+#except:
+ #   print("Unable to connect to database")
 
 
 def main():
@@ -30,29 +31,51 @@ def main():
     height= login_window.winfo_screenheight()   
     myfont = ('Inter')
 
+    #insert image
+    image=Image.open('main_wallpaper.png')
+    my_img = image.resize((width, height))
+    background_img = ImageTk.PhotoImage(my_img)
+    can = Canvas(login_window,width=width, height=height)
+    can.pack(expand="true", fill="both")
+    can.create_image(0,0,image=background_img, anchor = "nw")
+
+    #create window
     login_window.geometry("%dx%d" % (width, height))
-    login_window.configure(bg='#D2DAFF')
-    greeting = Label(login_window, text="Login", pady=40, background="#D2DAFF", font = (myfont, 20, 'bold'))
-    greeting.pack()
+    #login_window.configure(bg='#D2DAFF')
+    label_colour = "#e1ddd7"
+    button_colour = "#f9f6f6"
+    title = Label(login_window, text="Expense Tracker",  background=label_colour, font = (myfont, 24, 'bold'))
+    title.place(x=900, y = 60)
+
+    greeting = Label(login_window, text="Login", background=label_colour, font = (myfont, 19, 'bold'))
+    greeting.place(x=950, y = 150)
 
     email_text = StringVar() #stores string
-    l1 = Label(login_window,text="Email:", font = (myfont, 14), background="#D2DAFF")
-    l1.pack(pady=10)
+    l1 = Label(login_window,text="Username:", font = (myfont, 14), background=label_colour)
+    l1.place(x=950,y=200)
     e1 = Entry(login_window, textvariable=email_text, font = (myfont, 12))
-    e1.pack()
+    e1.place(x=950,y=235)
     
     password_text = StringVar()
-    l2 = Label(login_window,text="Password:", font = (myfont, 14), background="#D2DAFF")
-    l2.pack(pady = 10)
+    l2 = Label(login_window,text="Password:", font = (myfont, 14), background=label_colour)
+    l2.place(x=950,y=280)
     e2 = Entry(login_window, textvariable = password_text, show='*', font = (myfont, 12))
-    e2.pack()
+    e2.place(x=950,y=315)
+    
+    b1 = Button(login_window, command=login_database, width = 15, text="Login", font = (myfont, 12),bg = button_colour)
+    b1.place(x=965,y=360)  
+    
+    l3 = Label(login_window, font = (myfont, 12, 'italic'), background=label_colour)
+    l3.place(x=950,y=395)   
 
-    l3 = Label(login_window, font = (myfont, 12, 'italic'), background="#D2DAFF")
-    l3.pack(pady=5)
+    l4 = Label(text= 'Not an existing', font =(myfont, 11, 'italic'), background=label_colour)
+    l4.place(x= 945, y=430)
 
-    b = Button(login_window, command=login_database, text="Login", font = (myfont, 12),bg = '#F3F8FF')
-    b.pack(pady=10)
+    l5 = Label(text= 'user?', font =(myfont, 11, 'italic'), background=label_colour)
+    l5.place(x= 945, y = 450)
 
+    b2 = Button(login_window, command=signup, text="Sign Up", font = (myfont, 11),bg = button_colour)
+    b2.place(x=1050,y=436)  
     login_window.mainloop()
     
 #Actions on Pressing Signup button
@@ -66,46 +89,48 @@ def signup():
             cur.execute(user_query,user_tup)
             conn.commit()  
 
-            l4 = Label(signup_window,text="Account created successfully!", background="#D2DAFF", font=(myfont, 12, 'italic'))
+            l4 = Label(signup_window,text="Account created successfully!", background=label_colour, font=(myfont, 12, 'italic'))
             l4.place(x= 170,y=300)
             
         else:
-            l4 = Label(signup_window, width = 30, text="Enter valid information!", background="#D2DAFF", font=(myfont, 12, 'italic'))
+            l4 = Label(signup_window, width = 30, text="Enter valid information!", background=label_colour, font=(myfont, 12, 'italic'))
             l4.place(x= 150,y=300)
 
     #create a new window for signup process
     signup_window = Tk() 
     signup_window.title("Sign Up") 
-    signup_window.geometry("%dx%d" % (width, height))
-    signup_window.configure(bg='#D2DAFF')
+    signup_window.geometry("450x350")
+    signup_window.configure(bg='#e1ddd7')
 
-    greeting = Label(signup_window, text="Sign Up", pady=40, background="#D2DAFF", font = (myfont, 20, 'bold'))
+    label_colour = "#e1ddd7"
+    button_colour = "#f9f6f6"
+
+    greeting = Label(signup_window, text="Sign Up", pady=40, background=label_colour, font = (myfont, 20, 'bold'))
     greeting.pack() 
-
-    l1 = Label(signup_window,text="Username:", font = (myfont, 14), background="#D2DAFF")
-    l1.place(x=125,y=100)
+    
     name_text = StringVar() #declaring string variable for storing name and password
+    l1 = Label(signup_window,text="Username:", font = (myfont, 14), background=label_colour)
+    l1.place(x=100,y=100)
     e1 = Entry(signup_window,textvariable=name_text, font = (myfont, 12))
-    e1.place(x=225,y=100)
-
-    l2 = Label(signup_window,text="Email:", font = (myfont, 14), background="#D2DAFF")
-    l2.place(x=125,y=160)
+    e1.place(x=200,y=100)
+    
     email_text = StringVar()
+    l2 = Label(signup_window,text="Email:", font = (myfont, 14), background=label_colour)
+    l2.place(x=100,y=160)
     e2 = Entry(signup_window,textvariable=email_text, font = (myfont, 12))
-    e2.place(x=225,y=160)
-
-    l3 = Label(signup_window,text="Password:", font = (myfont, 14), background="#D2DAFF")
-    l3.place(x=125,y=220)
+    e2.place(x=200,y=160)
+    
     password_text = StringVar()
+    l3 = Label(signup_window,text="Password:", font = (myfont, 14), background=label_colour)
+    l3.place(x=100,y=220)
     e3 = Entry(signup_window,textvariable=password_text,show='*', font = (myfont, 12))
-    e3.place(x=225,y=220)
+    e3.place(x=200,y=220)
 
-    b = Button(signup_window,text="signup", command=signup_database, font = (myfont, 12),bg = '#F3F8FF')
-    b.pack(pady=10)
+    b = Button(signup_window,text="Sign Up", command=signup_database, font = (myfont, 12),bg = button_colour)
+    b.place(x=225,y=260)
 
     signup_window.mainloop()
-    
-    
+      
 def home():
     #table creation
     cur.execute('''CREATE TABLE IF NOT EXISTS expenses(SNO INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -119,7 +144,7 @@ def home():
     home_window = Tk()
     home_window.title('Registration')
     home_window.geometry("%dx%d" % (width, height))
-    home_window.configure(bg='#CDF0EA')
+    home_window.configure(bg='#dcd3e1')
  
     def addExpense(username):
         date = input('Date (yyyy-mm-dd):')
